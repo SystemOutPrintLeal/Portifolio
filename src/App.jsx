9b21f4347468aa495fd9ca6698930ab1005a3fe4
victor leal martins <victorleal@discente.ufg.br>
2022-11-28 21:48:13 -0300

    creating project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
-  return (
-          <>Learn React</>
-  );
-}
+import { createGlobalStyle } from 'styled-components'
+import Header from './components/Header'
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+const App = () => (
+  <>
+    <GlobalStyle/>
+    <Header/>
+    <div>portifolio netflix theme</div>
+  </>
+)
+
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "./style.js";
+
+const Header = () => (
+    <Container>Header</Container>
+)
+
+
+export default Header
