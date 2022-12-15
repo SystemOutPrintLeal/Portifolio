5de1fc2f81864578d8098406aa9124a379c3cba8
victor leal martins <victorleal@discente.ufg.br>
2022-12-14 22:04:02 -0300

    creating skills translate effect

--- src/components/Sections/index.jsx
+++ src/components/Sections/index.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import {
     Container,
     ButtonLetf, 
-    ButtonRight
+    ButtonRight,
+    SkillWrapper, 
+    Skill
     } from './style'
 
 import {
@@ -12,12 +14,31 @@ import {
 
 const Sections = () => {
 
+    const foo = [0,0,0,0,0]
+
     return(
         <Container>
             <h1>Name</h1>
             <ButtonLetf type="button">
                 <FaChevronLeft />
             </ButtonLetf>
+
+            <SkillWrapper style={{ marginLeft: 0, width: 1100}}>
+                {foo.map((f)=>(
+                    <Skill>
+                        <img
+                            src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1uS9tYSYXU5jshLx2WlnOLZrMgD.jpg`}
+                            alt={`Skill`}/>
+
+                        
+                        
+                    </Skill>
+                ))}
+        
+    
+
+            </SkillWrapper>
+
         </Container>
     )
 }
--- src/components/Sections/style.js
+++ src/components/Sections/style.js
@@ -22,12 +22,15 @@ export const Container = styled.div`
         z-index: 6;
         background: rgba(20, 20, 20, 0.8);
         border: 0;
+        
         svg {
           width: 40px;
           height: 40px;
           color: #fff;
         }
+
         visibility: hidden;
+
         &:hover {
           svg {
             width: 45px;
@@ -35,12 +38,58 @@ export const Container = styled.div`
           }
         }
       }
+
+      &:hover {
+        button {
+          visibility: visible;
+        }
+      }
 `
+
+export const SkillWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    transition: margin-left 1s;
+`
+
+export const Skill = styled.div`
+    position: relative;
+    height: auto;
+    width: 200px;
+
+    img {
+        position: relative;
+        z-index: 5;
+        width: 100%;
+        transform: scale(0.9);
+        transition: all ease 0.6s;
+      }
+
+      &:hover {
+        cursor: pointer;
+        > div {
+          top: 0;
+          left: 10px;
+          z-index: 10;
+          width: 400px;
+          height: 300px;
+          transform: scale(1) translateX(90px);
+          opacity: 1;
+          border-radius: 0 8px 8px 0;
+        }
+        img {
+          border-radius: 8px 0 0 8px;
+          transform: scale(1) translateX(-100px);
+        }
+      }
+`
+
 export const ButtonLetf = styled.button`
   left: 0;
-`;
+`
 export const ButtonRight = styled.button`
   right: 0;
-`;
+`
 
-export default { Container, ButtonLetf, ButtonRight }
+export default { Container, ButtonLetf, ButtonRight, SkillWrapper, Skill }
