64135192616532fa11353461406c9770bda9d14f
victor leal martins <victorleal@discente.ufg.br>
2023-02-13 22:58:16 -0300

    add construction flag

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Header from '../../components/Header'
 import Featured from '../../components/Featured'
 import Sections from '../../components/Sections'
 import { backend, frontend } from '../../services/skills'
+import { FaHammer } from 'react-icons/fa';
 
+const ConstructionFlag = (props) => (
+    <div style={{
+        transition:'all 0.8', 
+        backgroundColor: '#d3d3d3',
+        color: 'black',
+        display: props.isVisible,
+        justifyContent: 'center'}}>
+            <p style={{fontSize:'18px' , margin: '2px'}}><FaHammer/> Em Construção...<FaHammer/></p>
+    </div>
+)
 
 export default function Home(){
 
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener('scroll', () => setIsVisible(window.scrollY > 10));
+
+        return () => {
+            window.removeEventListener('scroll', () =>
+            setIsVisible(window.scrollY > 10))
+        }
+    },[])
+
+
     return(
         <>
+            <ConstructionFlag isVisible={isVisible?'none':'flex'}/>
             <Header/>
             <Featured/>
             <Sections title="Backend" skills ={backend}/>
             <Sections title="Frontend" skills ={frontend}/>
-
+            <ConstructionFlag isVisible={'flex'}/>
 
         </>
     )
